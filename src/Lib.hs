152e984687ae25063e473de8500c3872a448d1a3
keinsell <keinsell@protonmail.com>
2025-07-25 19:09:45 +0200

    add `parseScope`

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,20 +4,41 @@ import Text.Parsec
 import Text.Parsec.String (Parser)
 
 -- ^ A <verb> describes how something has changed via an expectation. An expectation is a requirement that the code should respect. The Standard Commits format provides a set of predefined verbs to ensure consistency and clarity in commit messages, and other verbs SHOULD be avoided.
+
 data Verb
-  = Add -- ^ Adds an expectation Introduces new content to the repository with the expectation that it SHALL behave as intended.
+  = -- | Adds an expectation Introduces new content to the repository with the expectation that it SHALL behave as intended.
+    Add
   | Remove
   | Refactor
   | Fix
   | Undo
-  | Release deriving (Show, Eq)
+  | Release
+  deriving (Show, Eq)
+-- ^ Implies the change MUST NOT be particularly relevant for maintainers or users. This field is a marker that is intended to be applied only to specific commits that maintainers/users should pay attention to.
 
 data Importance
-    = PossiblyBreaking -- ^ `?` (question) Changes something exposed externally, but not an API. It SHOULD NOT be breaking for projects that depend on the underlying repository.
-    | Breaking -- ^ `!` (exclamation) Changes something exposed externally, and MAY break projects that depend on the underlying repository.
-    | Critical deriving (Show, Eq) -- ^ `!!` (loud exclamation) This change is critical ─ previous versions have severe issues that must be addressed. Projects depending on the underlying repository SHOULD update immediately. The <footer> MUST specify the last safe commit (Last-safe-commit).
+  = -- | `?` (question) Changes something exposed externally, but not an API. It SHOULD NOT be breaking for projects that depend on the underlying repository.
+    PossiblyBreaking
+  | -- | `!` (exclamation) Changes something exposed externally, and MAY break projects that depend on the underlying repository.
+    Breaking
+  | -- | `!!` (loud exclamation) This change is critical ─ previous versions have severe issues that must be addressed. Projects depending on the underlying repository SHOULD update immediately. The <footer> MUST specify the last safe commit (Last-safe-commit).
+    Critical
+  deriving
+    ( Show,
+      Eq
+    )
 
-data Scope = Executable | BackendLibrary | Testing | Building | Documentation | ContinousIntegration | ContinuousDelivery
+data Scope
+  = -- | `exe` (executable) The change affects the executable part of the project.
+    Executable (Maybe String)
+  | BackendLibrary (Maybe String)
+  | Testing (Maybe String)
+  | Building (Maybe String)
+  | Documentation (Maybe String)
+  | ContinousIntegration (Maybe String)
+  | ContinuousDelivery (Maybe String)
+  deriving
+    (Show, Eq)
 
 data Reason = Introduction | Preliminary | Efficiency | Reliability | Compatibility | Temporary | Experiment | Security | Upgrade | UserExperience | Policy | Styling
 
@@ -26,24 +47,28 @@ data Reason = Introduction | Preliminary | Efficiency | Reliability | Compatibil
 -- <body?>
 --
 -- <footer?>
-data CommitMessage = CommitMessage {
-    verb :: Verb,
-    importance :: Maybe Importance
-} deriving (Show, Eq)
+data StandardCommit = StandardCommit
+  { verb :: Verb,
+    importance :: Maybe Importance,
+    summary :: String,
+    scope :: Maybe Scope
+  }
+  deriving (Show, Eq)
+
+type CommitMsg = String
 
 parseVerb :: Parser Verb
-parseVerb =
-  -- ? For sure this can be abstracted futher with map or smth
-  choice
-    [ try (string "refactor" *> pure Refactor),
-      try (string "release" *> pure Release),
-      try (string "remove" *> pure Remove),
-      try (string "add" *> pure Add),
-      try (string "undo" *> pure Undo),
-      try (string "fix" *> pure Fix),
-      try (string "rem" *> pure Remove),
-      try (string "ref" *> pure Refactor)
-    ]
+parseVerb = do
+  verb <-
+    choice
+      [ string "add" *> pure Add,
+        string "remove" *> pure Remove,
+        string "refactor" *> pure Refactor,
+        string "fix" *> pure Fix,
+        string "undo" *> pure Undo,
+        string "release" *> pure Release
+      ]
+  pure verb
 -- ^ Parses the importance of a commit message, which can be one of the
 -- following: `?`, `!`, `!!`, or nothing.
 -- If it is not specified, it defaults to `Nothing`.
@@ -51,7 +76,6 @@ parseVerb =
 parseImportance :: Parser (Maybe Importance)
 parseImportance =
   do
-    -- ? For sure this can be abstracted futher with map or smth
     choice
       [ string "?" *> pure (Just PossiblyBreaking),
         string "!" *> pure (Just Breaking),
@@ -66,34 +90,29 @@ parseScope =
     _ <- char '('
     scopeKind <-
       choice
-        [ try (string "exe" *> pure Executable),
-          try (string "lib" *> pure BackendLibrary),
-          try (string "test" *> pure Testing),
-          try (string "build" *> pure Building),
-          try (string "docs" *> pure Documentation),
-          try (string "ci" *> pure ContinousIntegration),
-          try (string "cd" *> pure ContinuousDelivery)
+        [ string "exe" *> pure Executable,
+          string "lib" *> pure BackendLibrary,
+          string "test" *> pure Testing,
+          string "build" *> pure Building,
+          string "docs" *> pure Documentation,
+          string "ci" *> pure ContinousIntegration,
+          string "cd" *> pure ContinuousDelivery
         ]
-    pure verb
+    mSub <- optionMaybe (char '/' *> many1 (noneOf ")[]:"))
+    _ <- char ')'
+    pure $ scopeKind mSub
 
-
-parseImportance :: Parser (Maybe Importance)
-parseImportance = do
-    choice [
-        string "?" *> pure (Just PossiblyBreaking),
-        string "!" *> pure (Just Breaking),
-        string "!!" *> pure (Just Critical)
-        ]
-    <|> pure Nothing
-
-parseCommitMessage :: Parser CommitMessage
+parseCommitMessage :: Parser StandardCommit
 parseCommitMessage = do
-    verb <- parseVerb
-    importance <- parseImportance
-    pure $ CommitMessage verb importance
+  verb <- parseVerb
+  importance <- parseImportance
+  scope <- parseScope
+  --   Handle newline after first line
+  pure $ StandardCommit verb importance "" scope
 
-parseStandardCommitMessage :: String -> IO ()
-parseStandardCommitMessage commitMsg = do
-    case parse parseCommitMessage "CommitMsg" commitMsg of
-        Left err -> putStrLn $ "Error: " ++ show err
-        Right msg -> print msg
+-- | Parses a commit message in the Standard Commits format.
+parseStandardCommitMessage :: CommitMsg -> Either ParseError StandardCommit
+parseStandardCommitMessage commitMsg =
+  case parse parseCommitMessage "CommitMsg" commitMsg of
+    Left err -> Left err
+    Right msg -> Right msg
