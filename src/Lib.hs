4fe49084607a6ec035bf2f5577ab8e604de52e85
keinsell <keinsell@protonmail.com>
2025-07-25 17:01:26 +0200

    add `parseVerb`

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,20 +3,44 @@ module Lib where
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
-data Verb = Add |  Remove | Refactor | Fix | Undo | Release
+-- ^ A <verb> describes how something has changed via an expectation. An expectation is a requirement that the code should respect. The Standard Commits format provides a set of predefined verbs to ensure consistency and clarity in commit messages, and other verbs SHOULD be avoided.
+data Verb
+  = Add -- ^ Adds an expectation Introduces new content to the repository with the expectation that it SHALL behave as intended.
+  | Remove
+  | Refactor
+  | Fix
+  | Undo
+  | Release deriving (Show, Eq)
+
 data Importance = PossiblyBreaking | Breaking | Critical
-data Scope = Executable | BackendLibrary | Testing | Building | Documentation | ContinousIntegration | ContinousDelivery
+data Scope = Executable | BackendLibrary | Testing | Building | Documentation | ContinousIntegration | ContinuousDelivery
 data Reason = Introduction | Preliminary | Efficiency | Reliability | Compatibility | Temporary | Experiment | Security | Upgrade | UserExperience | Policy | Styling
 
+-- <verb><importance?>(<scope?>)[<reason?>]: <summary>
+--
+-- <body?>
+--
+-- <footer?>
 data CommitMsg = CommitMsg {
-    verb :: Verb,
-    importance :: Maybe Importance,
-    scope :: Maybe Scope,
-    reason :: Maybe Reason,
-    summary :: Maybe String
-}
+    verb :: Verb
+} deriving (Show, Eq)
 
+
+
+parseVerb :: Parser Verb
+parseVerb = do
+    verb <- choice [
+        string "add" *> pure Add,
+        string "remove" *> pure Remove,
+        string "refactor" *> pure Refactor,
+        string "fix" *> pure Fix,
+        string "undo" *> pure Undo,
+        string "release" *> pure Release
+        ]
+    pure verb
 
 parseStandardCommitMessage :: String -> IO ()
 parseStandardCommitMessage commitMsg = do
-    putStrLn commitMsg
+    case parse parseVerb "CommitMsg" commitMsg of
+        Left err -> putStrLn $ "Error: " ++ show err
+        Right msg -> print msg
