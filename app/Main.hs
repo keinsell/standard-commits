006afaefd7af92719d1ad3370c0d40accc6ef879
keinsell <keinsell@protonmail.com>
2025-07-25 19:09:45 +0200

    add `parseScope`

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,13 @@ module Main (main) where
 
 import Lib
 
+commitMessage = "add!(lib/type-check)[rel]: enforce type checking in function calls"
+
 main :: IO ()
-main = parseStandardCommitMessage "add!(lib/type-check)[rel]: enforce type checking in function calls"
+main =
+  case parseStandardCommitMessage commitMessage of
+    Left err -> putStrLn $ "Error: " ++ show err
+    Right msg -> do
+      putStrLn $ "Commit message: " ++ commitMessage
+      putStrLn "--------------"
+      putStrLn $ show msg
